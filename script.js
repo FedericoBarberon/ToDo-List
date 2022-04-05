ee57b7a77333daef30ab3ce2dde374da5e6f9577
Federico Barberon <66987826+FedericoBarberon@users.noreply.github.com>
2022-04-05 17:12:14 -0300

    v2

--- script.js
+++ script.js
@@ -1,23 +1,44 @@
 const form = document.getElementById("taskForm")
 const tasks = document.getElementById("tasks")
+const dataNum = document.getElementById("taskNum");
+const dataComplete = document.getElementById("taskComplete");
+let taskNum = 0;
+let tasksComplete = 0;
 
 form.addEventListener("submit", ev => {
     ev.preventDefault()
-
-    let newTask = ev.target.task.value
-
-    if(!tasks.childNodes[0]?.hasChildNodes()) showTasksList(true)
-    addNewTask(newTask)
+    
+    const newTask = ev.target.task.value.trim();
+    form.task.value = "";
+    
+    if(newTask) {
+        if(!tasks.firstElementChild?.hasChildNodes()) showTasksList(true)
+        addNewTask(newTask)
+        tasks.scrollTo(0,tasks.scrollHeight);
+    };
 })
 
 tasks.addEventListener("click", ev => {
     if(ev.target.tagName === "BUTTON"){
         deleteTask(ev.target.parentElement)
-        if(!tasks.childNodes[0]?.hasChildNodes()) showTasksList(false)
+        if(!tasks.firstElementChild?.hasChildNodes()) showTasksList(false)
+    }
+})
+
+tasks.addEventListener("click", ev => {
+    if(ev.target.tagName === "INPUT"){
+        if(ev.target.checked){
+            tasksComplete += 1;
+        } else {
+            tasksComplete -= 1;
+        }
+        updateData();
     }
 })
 
 const addNewTask = (task) => {
+    taskNum += 1;
+    updateData();
     let tasksList;
     if(tasks.childElementCount === 0) {
         tasksList = document.createElement("ul")
@@ -31,6 +52,12 @@ const addNewTask = (task) => {
     const newTask = document.createElement("li")
     newTask.classList.add("task")
 
+    const taskCheckBox = document.createElement("input")
+    taskCheckBox.setAttribute("type","checkbox")
+    taskCheckBox.name = "check"
+    taskCheckBox.classList.add("task__check")
+    taskCheckBox.setAttribute("title","Check Task")
+
     const taskContent = document.createElement("span")
     taskContent.classList.add("task__content")
     taskContent.textContent = task;
@@ -38,19 +65,29 @@ const addNewTask = (task) => {
     const deleteButton = document.createElement("button")
     deleteButton.classList.add("task__deleteButton")
     deleteButton.textContent = "x"
+    deleteButton.setAttribute("title","Delete Task")
 
+    newTask.appendChild(taskCheckBox)
     newTask.appendChild(taskContent)
     newTask.appendChild(deleteButton)
 
     tasksList.appendChild(newTask);
-    form.task.value = "";
 }
 
 const deleteTask = (task) => {
-    tasks.childNodes[0].removeChild(task)
+    taskNum -= 1;
+    if(task.firstElementChild.checked) tasksComplete -= 1;
+    updateData();
+    tasks.firstElementChild.removeChild(task);
 }
 
 const showTasksList = (show) => {
     if(show) tasks.classList.remove("hidden")
     else tasks.classList.add("hidden")
+}
+
+
+const updateData = () => {
+    dataNum.textContent = `${taskNum} Tasks`;
+    dataComplete.textContent = `${tasksComplete}/${taskNum} Complete`
 }
